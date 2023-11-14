fde55a5d4e7cb1f5ca31081d9d50820b758a8a98
Silvestrs <46748607+SilvisPilvis@users.noreply.github.com>
2023-11-14 17:32:10 +0200

    Add files via upload

--- Classes.cpp
+++ Classes.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
 using namespace std;
 
-class Dog {
+class Animal{
     public:
         string name;
-
-        Dog(string n, int a){
+        void birthday(){
+            age++;
+        };
+        virtual void makeSound() = 0;
+    protected:
+        int age;
+        Animal(string n, int a){
             name = n;
             age = a;
         }
+
+};
+
+class Dog : Animal{
+    public:
+        Dog(string name, int age) : Animal(name, age){};
         
         void birthday(){
             age++;
         }
         
-        void woof(){
-            cout << "Woof!";
+        void makeSound(){
+            cout << "Woof!\n";
         }
+};
 
-    private:
-        int age;
+class Cat : Animal{
+    public:
+        Cat(string name, int age) : Animal(name, age){};
+        
+        void birthday(){
+            age++;
+        }
+        
+        void makeSound(){
+            cout << "Meow!\n";
+        }
 };
 
 int main() {
-    
     Dog reksis("Reksis", 6);
-    reksis.woof();
-
+    reksis.makeSound();
+    Cat mincis("Mincis", 2);
+    mincis.makeSound();
+    Animal wierd();
     return 0;
 }
